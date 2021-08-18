1b4efd8a9cf45db9e07043761939ce543457a8f5
mondai40 <tvwmdaimon40@gmail.com>
2021-08-18 22:55:15 +0900

    refactor components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,34 +1,19 @@
-import 'package:angular/angular.dart';
-import 'package:angular_app/src/hero_component.dart';
-import 'package:angular_app/src/hero_service.dart';
-// import 'package:angular_app/src/mock_heros.dart';
-import 'package:angular_forms/angular_forms.dart';
 
-import 'src/hero.dart';
+import 'package:angular/angular.dart';
+
+
+import 'package:angular_app/src/hero_list_component.dart';
+import 'package:angular_app/src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
-  templateUrl: 'app_component.html',
-  styleUrls: ['app_component.css'],
-  directives: [coreDirectives, formDirectives, HeroComponent],
+  template: '''
+    <h1>{{ title }}</h1>
+    <my-heros></my-heros>
+  ''',
+  directives: [HeroListComponent],
   providers: [ClassProvider(HeroService)]
 )
-class AppComponent implements OnInit{
+class AppComponent {
   final String title = 'Tour of Heroes';
-  Hero selectedHero;
-  final HeroService _heroService;
-
-  AppComponent(this._heroService);
-
-  List<Hero> heros;
-  Future<void> _getHeros() async {
-    // _heroService.getAll()
-    //   .then((heros) => this.heros = heros);
-    heros = await _heroService.getAllSlowly();
-  }
-
-  void onSelect(Hero hero) => selectedHero = hero;
-
-  @override
-  void ngOnInit() => _getHeros();
 }
